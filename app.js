e9a84ebe4b4db21ab929c0c8cd141f23f4fdb9b7
kopalite <ivan.kopcanski@hotmail.com>
2017-10-26 12:02:40 +0400

    server.js added for openode.io integration

--- app.js
+++ app.js
@@ -2,14 +2,44 @@
 
 var express = require("express");
 var bodyParser = require("body-parser");
+var compress = require('compression');
+
 var app = express();
+
+app.use(compress());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//getting routes
-var routes = require("./routes/routes.js")(app);
+//settin routes
+require("./routes/routes.js")(app);
+
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    var html = '<!DOCTYPE html>';
+    html+= '<html>';
+    html+= '  <head>';
+    html+= '    <title></title>';
+    html+= '  </head>';
+    html+= '  <body>';
+    html+= '    <h1>'+err.message+'</h1>';
+    html+= '    <h2>'+err.status+'</h2>';
+    html+= '    <pre>'+err.stack+'</pre>';
+    html+= '  </body>';
+    html+= '</html>';
+    res.send(html);
+}); 
 
 //starting the server
-var server = app.listen(3000, function () {
-    console.log("inoutlog.server: listening on port %s...", server.address().port);
-});
+// var server = app.listen(3000, function () {
+//     console.log("inoutlog.server: listening on port %s...", server.address().port);
+// });
+
+module.exports = app;
